5788c6ec8d3f87649ef6041843258e694921e7c0
tittieth <114925706+tittieth@users.noreply.github.com>
2023-04-24 08:46:10 +0200

    send conclusion to db

--- models/conclusion-model.js
+++ models/conclusion-model.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const ConclusionSchema = new mongoose.Schema({
   colors: {
-    type: [String],
+    type:[[String]],
     required: true,
   },
   ids: {
     type:[[String]],
     required: true,
   },
-})
+});
 
-module.exports = mongoose.model('conclusion', ConclusionSchema)
+module.exports = mongoose.model('conclusion', ConclusionSchema);
